700629f2e0d2653d4309018b5ae470ba6b774dbf
pedro <130769202+phbrugnolo@users.noreply.github.com>
2024-04-19 20:39:14 -0300

    Add files via upload

---
+++ Retangulo/retangulo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple-v1.css">
+    <title>Calculado</title>
+</head>
+
+<body>
+    <center>
+        <?php
+
+        $lado1 = $_GET['lado1'];
+        $lado2 = $_GET['lado2'];
+
+        if ($lado1 > 0 and $lado2 > 0) {
+            echo "<h3>A area do retangulo é " . $lado1 * $lado2;
+            echo "<br><h3>E o seu perimetro é de " . $lado1 + $lado2;
+        } else {
+            echo "<h3>Voce digitou um valor inválido";
+        }
+
+        ?>
+        <a href="javascript:history.go(-1)">
+            <h2>Voltar para a página anterior</h2>
+        </a>
+        </p>
+    </center>
+</body>
+
+</html>
